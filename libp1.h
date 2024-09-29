53a34ffe29bbe6ab86aed3bb0c287f5d6a2f5ea5
alexborrazasm <alexborrazasm@gmail.com>
2024-09-29 19:20:24 +0200

    estructura funciones

--- libp1.h
+++ libp1.h
@@ -3,4 +3,31 @@
 
 #include "types.h" 
 
+// creates a file
+//void cmdMakefile(tArgs args, tLists *L);
+
+// creates a directory
+//void cmdMakedir(tArgs args, tLists *L);
+
+// gives information on files or directories
+//void cmdListfile(tArgs args, tLists *L);
+
+// prints current working directory
+//void cmdCwd(tArgs args, tLists *L); 
+
+// lists directories contents
+//void cmdListdir(tArgs args, tLists *L); 
+
+// lists directories recursively (subdirectories after)
+//void cmdReclist(tArgs args, tLists *L); 
+
+// lists directories recursively (subdirectories before)
+//void cmdRevlist(tArgs args, tLists *L); 
+
+// deletes files and/or empty directories
+//void cmdErase(tArgs args, tLists *L); 
+
+// deletes files and/pr non empty directories recursively
+//void cmdDelrec(tArgs args, tLists *L); 
+
 #endif // LIBP1_H
